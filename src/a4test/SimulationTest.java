f18acbe83ee75828f3739139deb1b3e401a32643
jemin <jemin@JEXPS>
2020-02-13 04:01:50 -0500

    Final - simulationtest cleaned

--- src/a4test/SimulationTest.java
+++ src/a4test/SimulationTest.java
@@ -1,10 +1,5 @@
 package a4test;
 
-import a4.CompletedRide;
-import a4.Position;
-import a4.RoundRobinDispatcher;
-import a4.Simulation;
-import a4.Vehicle;
 import a4.*;
 
 public class SimulationTest {
@@ -21,23 +16,5 @@ public class SimulationTest {
 			total_profit += profit;
 		}
 		System.out.println("Total Profit: " + String.format("%.2f", total_profit));
-//		completedRidePriceTest();
 	}
-	
-
-//	static void completedRidePriceTest() {
-//		Position driver_pos = new PositionImpl(0,0);
-//		Vehicle v = new VehicleImpl("Ford", "Focus", "ABC-123", driver_pos);
-//		Driver d = new DriverImpl("John", "Smith", 1234, v);
-//		
-//		RideRequest req = new RideRequestImpl(new PositionImpl(1,1), new PositionImpl(1,10));
-//		CompletedRide c = new CompletedRideImpl(req, d);
-//		req.complete(d);
-//		System.out.println(c.getWaitTime());
-//		System.out.println(c.getTotalTime());
-//		System.out.println(c.getPrice());
-//		System.out.println(c.getCost());
-//		System.out.println(c.getProfit());
-//		System.out.println(d.getVehicle().getPosition().getX() + ", " + d.getVehicle().getPosition().getY());
-//	}
 }
